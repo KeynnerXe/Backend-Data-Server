3cd7d0d158689959deb94740a56ed49925569c6a
Keinner <keinner.ramoszuniga@unicolombo.edu.co>
2025-09-16 17:50:54 -0500

    error fixed

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -12,3 +12,17 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = prisma;
+
+/* const { PrismaClient } = require('@prisma/client');
+
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+module.exports = prisma; */
