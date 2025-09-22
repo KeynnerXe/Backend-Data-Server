3f52c9443cc29df21ebd19be87f4b2a576bbacd3
Keinner <keinner.ramoszuniga@unicolombo.edu.co>
2025-09-22 16:56:20 -0500

    prisma.js update

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,18 +1,8 @@
-const { execSync } = require("child_process");
-
-try {
-  // Generar Prisma Client al vuelo
-  execSync("npx prisma generate", { stdio: "inherit" });
-  console.log("✅ Prisma Client generado al vuelo");
-} catch (e) {
-  console.warn("⚠️ No se pudo ejecutar prisma generate al vuelo:", e.message);
-}
-
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 
 let prisma;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
