2e9b7f81cb0af274ad11080ca2c38edf83f2c817
Keinner <keinner.ramoszuniga@unicolombo.edu.co>
2025-09-23 15:26:06 -0500

    server.js update fixed

--- src/server.js
+++ src/server.js
@@ -3,24 +3,24 @@ const session = require("express-session");
 const passport = require("passport");
 const cors = require("cors");
 const path = require("path");
+
+// Config y rutas
 const dashboardRoutes = require("./routes/dashboard.routes");
 const configurePassport = require("./config/passport");
-
-
-// Importar rutas
 const authRoutes = require("./routes/auth.routes");
 const taskRoutes = require("./routes/task.routes");
 const dataRoutes = require("./routes/data.routes");
-
-// 👇 Corrección aquí
 const prisma = require("./config/prisma");
 
 const app = express();
 
-//app.use(cors());
+// Middleware CORS
 app.use(cors({ origin: true, credentials: true }));
 
+// Middleware JSON
 app.use(express.json());
+
+// Middleware sesión
 app.use(
   session({
     secret: "mi_secreto",
@@ -29,11 +29,18 @@ app.use(
   })
 );
 
+// Passport
 app.use(passport.initialize());
 app.use(passport.session());
 configurePassport(passport);
 
-// Servir archivos estáticos desde la carpeta public
+// 👇 Middleware para evitar errores Range
+app.use((req, res, next) => {
+  res.setHeader("Accept-Ranges", "none");
+  next();
+});
+
+// Servir archivos estáticos
 app.use(express.static(path.join(__dirname, "../public")));
 
 // Rutas
@@ -42,6 +49,7 @@ app.use("/tasks", taskRoutes);
 app.use("/api/data", dataRoutes);
 app.use("/dashboard", dashboardRoutes);
 
+// Ruta base
 app.get("/", (req, res) => {
   res.send("Servidor funcionando.");
 });
@@ -49,8 +57,10 @@ app.get("/", (req, res) => {
 // Manejo global de errores
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Error interno del servidor', details: err.message });
+  res
+    .status(500)
+    .json({ error: "Error interno del servidor", details: err.message });
 });
-
+// Exportar app y handler para Vercel
 module.exports = app;
 module.exports.handler = (req, res) => app(req, res);
